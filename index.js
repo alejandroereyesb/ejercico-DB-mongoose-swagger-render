d0762dcebc6ef2ef1a7020535814e736e1ad5859
alejandroereyesb <reyesbalejandroe@gmail.com>
2026-02-02 21:08:24 +0100

    update/index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 3000;
 const { dbConnection } = require('./config/config');
 const routes = require('./routes');
 // Swagger
